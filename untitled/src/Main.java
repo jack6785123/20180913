82ddd71d817423befa735572c8c6789f285baf35
jack6785123 <32056027+jack6785123@users.noreply.github.com>
2018-10-04 11:45:07 +0800

    Add files via upload

---
+++ untitled/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        MainFrame2 fr = new MainFrame2();
+        fr.setVisible(true);
+    }
+}
---
+++ untitled/src/MainFrame3.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class MainFrame3 extends JFrame {
+    private JMenuBar jbr = new JMenuBar();
+    private JMenu jnu = new JMenu("file");
+    private  JMenu jnu1 = new JMenu("game");
+    private JMenuItem jit = new JMenuItem("OPEN");
+    private JMenuItem jit2 = new JMenuItem("EXIT");
+    private JMenuItem jit3 = new JMenuItem("about");
+    private JMenuItem OX =new JMenuItem("OX");
+    public   MainFrame3(){
+        inin();
+    }
+public void inin(){
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setBounds(500,500,500,500);
+        this.setLayout(null);
+        this.setJMenuBar(jbr);
+        jbr.add(jnu);
+        jbr.add(jnu1);
+        jbr.add(OX);
+        jnu.add(jit);
+        jnu.add(jit3);
+        jnu.add(jit2);
+        OX.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MainFrame4 mm = new MainFrame4(MainFrame3.this);
+                mm.setVisible(true);
+                MainFrame3.this.setVisible(false);
+            }
+        });
+        jit2.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+
+}
+}
+
